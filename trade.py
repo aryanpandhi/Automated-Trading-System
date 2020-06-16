cd643c0e0f190214de83479086109cd9f77c43d9
ap <aryan.pandhi@gmail.com>
2020-06-16 17:06:58 +0530

    config

--- trade.py
+++ trade.py
@@ -1,7 +1,6 @@
 import alpaca_trade_api as tradeapi
+from config import *
 
-APCA_API_KEY_ID = 'PKW28DNPAGUH4TDZIL73'
-APCA_API_SECRET_KEY = 'ecv8EUMV8zQmIilkUChHyOBRD/02eyYNVVfN03C6'
 APCA_API_BASE_URL = 'https://paper-api.alpaca.markets'
 
 api = tradeapi.REST(APCA_API_KEY_ID,APCA_API_SECRET_KEY,APCA_API_BASE_URL)
